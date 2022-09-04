85d980e19381aaec099d1947966d8eeee7100855
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-04 19:26:51 +0100

    Exercise-4.1

---
+++ Program8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iIndex;
+
+            for (iIndex = 3; iIndex <= 6; iIndex++)
+            {
+                Console.WriteLine(iIndex);
+            }
+
+            Console.WriteLine();
+
+            for (iIndex = 4; iIndex < 9; iIndex++)
+            {
+                Console.WriteLine(iIndex);
+            }
+
+            Console.WriteLine();
+
+            for (iIndex = (4+1); iIndex < 13; iIndex++)
+            {
+                Console.WriteLine(iIndex);
+            }
+        }
+    }
+}
