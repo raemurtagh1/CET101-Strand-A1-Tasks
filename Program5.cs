978896dfb147525c292bca50f4aaeca95a602b1d
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-04 19:12:14 +0100

    Exercise-2

---
+++ Program5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNumber = 5;
+            if (iNumber > 5)
+            {
+                iNumber = iNumber + 5;
+            }
+            else if (iNumber > 2)
+            {
+                iNumber = iNumber + 10;
+            }
+            else
+            {
+                iNumber = iNumber + 15;
+            }
+
+            Console.WriteLine(iNumber);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
