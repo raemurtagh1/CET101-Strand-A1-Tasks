1fbe2567812b4a4cfdaa7721cda7638112fa5007
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-04 19:32:07 +0100

    Exercise-4.3

---
+++ Program10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_4._3___AddFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iIndex, iVal, iTotal = 0;
+            for (iIndex = 1; iIndex <= 5; iIndex++)
+            {
+                Console.Write("Enter number: ");
+                iVal = Convert.ToInt32(Console.ReadLine());
+                iTotal = iTotal + iVal;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total = " + iTotal);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();  
+        }
+    }
+}
