7ce4f533064a4a39a9e7a096b134641cab4200a6
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-04 19:59:51 +0100

    Student-Details

---
+++ Program15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            String sFirstName;
+            String sLastName;
+            String sProgramme;
+
+            //Enter student first name
+            Console.Write("Please enter your first name: ");
+            sFirstName = Console.ReadLine();
+
+            //Enter student last name
+            Console.Write("Please enter your last name: ");
+            sLastName = Console.ReadLine();
+
+            //Enter programme name
+            Console.Write("Which programme are you studying? ");
+            sProgramme = Console.ReadLine();
+
+            //Saying hello to the student with the programme that they are from
+            Console.WriteLine();
+            Console.WriteLine("Hello " + sFirstName + " " + sLastName + " from the " + sProgramme + " programme!");
+
+            //Verifying programme closure
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
