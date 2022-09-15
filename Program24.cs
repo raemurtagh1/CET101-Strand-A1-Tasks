d3c937fcebe5c4a442cb0e3a0e48a2a9ef8c97c6
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-15 01:30:04 +0100

    Wedding-Invitation

---
+++ Program24.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WeddingInvitation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sGuest;
+            String sBride;
+            String sGroom;
+
+            Console.Write("Please enter the name of the guest: ");
+            sGuest = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Please enter the name of the bride: ");
+            sBride = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Please enter the name of the groom: ");
+            sGroom = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Wedding Invitation = ");
+            Console.WriteLine("");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("                      " + sGuest);
+            Console.WriteLine("             " + "is invited to the wedding of:");
+            Console.WriteLine("               " + sBride + " and " + sGroom);
+            Console.WriteLine("                on Saturday 17th July at 2pm");
+            Console.WriteLine();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
