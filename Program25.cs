957c5f648f02f0547cc7bd5f583ef3e84aed2c2c
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-16 03:53:19 +0100

    switch-practice-task

---
+++ Program25.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace switch-practice-task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iChoice;
+
+            Console.Write("Enter an integer: ");
+            iChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (iChoice)
+            {
+                case 1:
+                case 3:
+                case 5:
+                {
+                    Console.WriteLine("Odd number");
+                    break;
+                }
+
+                case 2:
+                case 4:
+                case 6:
+                {
+                    Console.WriteLine("Even number");
+                    break;
+                }
+
+                default:
+                {
+                    Console.WriteLine("Number higher than six");
+                    break;
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
